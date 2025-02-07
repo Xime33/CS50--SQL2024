1a31c17da06149199074a74da91c0f478ba1dc66
Xime33 <113552320+Xime33@users.noreply.github.com>
2025-02-07 13:05:23 -0600

    Add files via upload

---
+++ players/10.sql
@@ -0,0 +1,1 @@
+SELECT "first_name" AS "NOMBRE", "last_name" AS "Apellido" FROM "players" WHERE "birth_country" LIKE "%MEX%" ORDER BY "last_name" ASC;
---
+++ players/6.sql
@@ -0,0 +1,1 @@
+SELECT "debut", "first_name", "last_name" FROM "players" WHERE ("birth_city" LIKE "Pittsburgh") ORDER BY "debut" DESC, "first_name" , "last_name" ;
